13f4cda9382325d65ee07aa4d1b39e5104da1413
John Malhinha <john.malhinha@gmail.com>
2021-05-11 19:43:39 -0700

    created person class

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+// task 1: create Hamster class
 class Hamster {
   constructor(name) {
     this.owner = '';
@@ -15,3 +16,48 @@ class Hamster {
     return this.price;
   };
 };
+
+// task 2: create Person class
+class Person {
+  constructor(name) {
+    this.name = name;
+    this.age = 0;
+    this.height = 0;
+    this.weight = 0;
+    this.mood = 0;
+    this.hamsters = [];
+    this.bankAccount = 0;
+  }
+
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+  getWeight() {
+    return this.weight;
+  }
+  greet(name) {
+    console.log(`Hello ${name}`);
+  }
+  eat(num) {
+    this.weight+=num;
+    this.mood+=num;
+  }
+  exercise(num) {
+    this.weight-=num;
+  }
+  ageUp(num) {
+    this.age+=num;
+    this.height+=num;
+    this.weight+=num;
+    this.mood-=num;
+    this.bankAccount+=(10*num);
+  }
+  buyHamster(hamster) {
+    this.hamsters.push(hamster);
+    this.mood+=10;
+    this.bankAccount-=hamster.getPrice();
+  }
+}
