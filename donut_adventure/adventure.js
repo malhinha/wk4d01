06042611b07638a1cb02020462f71536d5e53c38
John Malhinha <john.malhinha@gmail.com>
2021-05-15 17:13:14 -0700

    created our enemy pizza rat

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -26,4 +26,34 @@ class Hero {
   }
 }
 
-const dougie = new Hero('Dougie the Donut'); 
+const dougie = new Hero('Dougie the Donut');
+
+//Part 2: create the enemy class
+
+class Enemy {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.weapons = {
+      pepperoniStars: 5,
+      cheeseGrease: 10,
+    };
+    this.catchPhrases = [
+      'i\'m youtube famous',
+      'i\'m more dangerous than an uncovered sewer'
+    ]
+  }
+
+  talkSmack() {
+    let num = Math.floor(Math.random() * (this.catchPhrases.length - 0));
+    console.log(this.catchPhrases[num]);
+  }
+  announceHealth() {
+    console.log(this.health);
+  }
+  fight() {
+    console.log(`i\'m gonna flatten you like a slice of pepperoni!`);
+  }
+}
+
+const pizzaRat = new Enemy('Pizza Rat');
